4baa265180e1d460d2e7724ec35c632f43cd0a0b
kodam <hotsoup.h@gmail.com>
2014-09-03 15:16:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Typotter - It's new $module
+
+# SYNOPSIS
+
+    use Typotter;
+
+# DESCRIPTION
+
+Typotter is ...
+
+# LICENSE
+
+Copyright (C) kodam.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kodam <hotsoup.h@gmail.com>
---
+++ lib/Typotter.pm
@@ -0,0 +1,39 @@
+package Typotter;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Typotter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Typotter;
+
+=head1 DESCRIPTION
+
+Typotter is ...
+
+=head1 LICENSE
+
+Copyright (C) kodam.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kodam E<lt>hotsoup.h@gmail.comE<gt>
+
+=cut
+
